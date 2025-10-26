806d518a325432f115b4994879532fef4b315217
Julian Scharf <julzelements@gmail.com>
2025-10-27 07:29:22 +1000

    yagni

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-export { MonologueDevice } from "./device/MonologueDevice";
 export { encodeMonologueParameters, decodeMonologueParameters, MonologueParameters } from "./sysex";
 
 // Prettification utilities - convert between numeric values and human-readable labels
-export * as prettify from "./sysex/prettify";
+export { prettyPanelSettings } from "./sysex/prettify";
